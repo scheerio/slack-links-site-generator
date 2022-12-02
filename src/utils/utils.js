11ea90f7c782c362f928fb5b0394290a520e3510
Ben Scheer <benjack321@gmail.com>
2022-12-02 11:35:40 -0500

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,33 +1,33 @@
-function getLinks(){
-    try {
-        const data = require('../data/current_links.json');
-        return data;
-    } catch(err) {
-        return null;
-    }
+function getLinks() {
+  try {
+    const data = require("../data/current_links.json");
+    return data;
+  } catch (err) {
+    return null;
+  }
 }
 
-function getRandomColor(){
-    return randomColors[Math.floor(Math.random() * randomColors.length)];
+function getRandomColor() {
+  return randomColors[Math.floor(Math.random() * randomColors.length)];
 }
 
 const randomColors = [
-    '#fff6e9',
-    '#ffefd7',
-    '#affef9',
-    '#e3f0ff',
-    '#a2d7ff',
-    '#c5e2ff', 
-    '#debefc',
-    '#D37EFF',
-    '#FFFDC6',
-    '#DFFFF7',
-    '#EdddF7',
-    '#ACDDDE',
-    '#CAF1DE',
-    '#E1F8DC',
-    '#FEF8DD',
-    '#FFE7C7'
-    ];
+  "#fff6e9",
+  "#ffefd7",
+  "#affef9",
+  "#e3f0ff",
+  "#a2d7ff",
+  "#c5e2ff",
+  "#debefc",
+  "#D37EFF",
+  "#FFFDC6",
+  "#DFFFF7",
+  "#EdddF7",
+  "#ACDDDE",
+  "#CAF1DE",
+  "#E1F8DC",
+  "#FEF8DD",
+  "#FFE7C7",
+];
 
 export { getRandomColor, getLinks };
